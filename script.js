d8d8067088f5e32bceb894e8c58885a15731e359
Jayden Stone <220652513+JaydenBStone@users.noreply.github.com>
2025-08-06 14:03:23 +1000

    Added btn and modal to begin the game. Added necessary js to allow it to function Added images within the modal.

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
+let computerChoice = 0;
+let humanChoice = 0;
+
+
+// Play Game Modal
+
+// DomMan
+var modal = document.getElementById('myModal');
+var myBtn = document.getElementById('myBtn');
+var span = document.getElementsByClassName('close')[0];
+// Display modal onclick "modal.style.display = 'block';"
+myBtn.onclick = function() {
+  modal.style.display = 'block';
+}
+// Close modal onclick of span(x) " modal.style.display = "none";"
+span.onclick = function() {
+  modal.style.display = "none";
+}
+// Close modal if click outside modal window " modal.style.display = "none";"
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+// Computer Choice Function
 function getComputerChoice() {
   function getRandomNumber() {
     return Math.floor(Math.random() * 3) + 1;
   }
-
   if (getRandomNumber() === 3) {
     console.log('three')
   } else if (getRandomNumber() === 2) {
@@ -11,5 +37,4 @@ function getComputerChoice() {
     console.log('one')
   }
 }
-
 getComputerChoice();
